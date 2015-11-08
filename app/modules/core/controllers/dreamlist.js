7eebebb854c3a3c5df3bdf52d7210e9c57f78d20
theStrategist <raaghav@outlook.com>
2015-11-07 19:13:42 -0800

    DreamList Version2

--- app/modules/core/controllers/dreamlist.js
+++ app/modules/core/controllers/dreamlist.js
@@ -25,19 +25,19 @@ angular
 				 },
 				
 				 {
-					title:"Great Wall Of China",
-					imagePath:"../../../great_wall_china.jpg",
-					content:"The Great Wall of China is a series of fortifications made of stone, brick, tamped earth, wood, and other materials, generally built along an east-to-west line across the historical northern borders of China to protect the Chinese states and empires against the raids and invasions of the various nomadic groups of the Eurasian Steppe."
+					title:"Yosemite",
+					imagePath:"../../../Yosemite.jpg",
+					content:"Yosemite National Park is set within California’s Sierra Nevada mountains. It’s famed for its giant, ancient sequoias, and for Tunnel View, the iconic vista of towering Bridalveil Fall and the granite cliffs of El Capitan and Half Dome."
 				 },
 
 				 {
-					title:"Great Wall Of China",
-					imagePath:"../../../great_wall_china.jpg",
-					content:"The Great Wall of China is a series of fortifications made of stone, brick, tamped earth, wood, and other materials, generally built along an east-to-west line across the historical northern borders of China to protect the Chinese states and empires against the raids and invasions of the various nomadic groups of the Eurasian Steppe."
+					title:"Paris Art Gallery",
+					imagePath:"../../../Paris.jpg",
+					content:"Paris has long been synonymous with burgeoning arts and cultural flair. With such a wealth of public galleries, creative events and white spaces, no selection of the top 10 contemporary art galleries can be comprehensive."
 				 },
 
 				 {
-					title:"Great Wall Of China",
+					title:"",
 					imagePath:"../../../great_wall_china.jpg",
 					content:"The Great Wall of China is a series of fortifications made of stone, brick, tamped earth, wood, and other materials, generally built along an east-to-west line across the historical northern borders of China to protect the Chinese states and empires against the raids and invasions of the various nomadic groups of the Eurasian Steppe."
 				 },
