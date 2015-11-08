c94ba9f863e30530f61c6f845d8bf16899baffc9
Pascal Crenzin <pascal.crenzin@sap.com>
2015-11-08 05:26:24 -0800

    dialog box for dreams

--- app/modules/core/controllers/dreamlist.js
+++ app/modules/core/controllers/dreamlist.js
@@ -5,67 +5,113 @@
  * @name core.Controllers.DreamlistController
  * @description DreamlistController
  * @requires ng.$scope
-*/
+ */
 angular
     .module('core')
     .controller('DreamlistController', [
-        '$scope',
-        function($scope) {
+        '$scope', '$mdDialog',
+        function ($scope, $mdDialog) {
 
-			$scope.dreams =
-				[{
-					title:"Dubai",
-					imagePath:"../img/dubai.jpg",
-					content:"",
-					visited:"true",
-                    options: ["Opt1", "Opt2", "Opt3"]
+            $scope.dreams = [{
+                    title: "Dubai",
+                    imagePath: "../img/dubai.jpg",
+                    content: "",
+                    visited: "true",
+                    dreams: ["huge tower", "Opt2", "Opt3"]
 
 				 },
 
-				 {
-					title:"Great Wall Of China",
-					imagePath:"../img/great_wall_china.jpg",
-					content:"",
-					visited:"false"
+                {
+                    title: "Great Wall Of China",
+                    imagePath: "../img/great_wall_china.jpg",
+                    content: "",
+                    visited: "false",
+                    dreams: ["huge tower", "Opt3"],
+                    price: 800
 				 },
 
-				 {
-					title:"Yosemite",
-					imagePath:"../img/Yosemite.jpg",
-					content:"",
-					visited:"false"
+                {
+                    title: "Yosemite",
+                    imagePath: "../img/Yosemite.jpg",
+                    content: "",
+                    visited: "false",
+                    dreams: ["huge tower", "Opt2", "Opt3"],
+                    price: 800
 				 },
 
-				 {
-					title:"Paris Art Gallery",
-					imagePath:"../img/Paris.jpg",
-					content:"",
-					visited:"false"
+                {
+                    title: "Paris Art Gallery",
+                    imagePath: "../img/Paris.jpg",
+                    content: "",
+                    visited: "false",
+                    dreams: ["huge tower", "Opt2", "Opt3"],
+                    price: 800
 				 },
 
-				 {
-					title:"Portugal",
-					imagePath:"../img/Portugal.jpg",
-					content:"",
-				 	visited:"false"
+                {
+                    title: "Portugal",
+                    imagePath: "../img/Portugal.jpg",
+                    content: "",
+                    visited: "false",
+                    dreams: ["huge tower", "Opt2", "Opt3"],
+                    price: 800
 				 },
 
 
-				 {
-					title:"The Nevis, Queenstown, New Zealand",
-					imagePath:"../img/Bungee_Jumping.jpg",
-					content:"",
-				 	visited:"true"
+                {
+                    title: "The Nevis, Queenstown, New Zealand",
+                    imagePath: "../img/Bungee_Jumping.jpg",
+                    content: "",
+                    visited: "true",
+                    dreams: ["huge tower", "Opt2", "Opt3"],
+                    price: 800
 				 },
 
-				 {
-					title:"Boom,Belgium",
-					imagePath:"../img/tomorrowland.jpg",
-					content:"",
-				 	visited:"false"
+                {
+                    title: "Boom,Belgium",
+                    imagePath: "../img/tomorrowland.jpg",
+                    content: "",
+                    visited: "false",
+                    dreams: ["huge tower", "Opt2", "Opt3"],
+                    price: 800
 				 }
 
-			    ]
+			    ];
+
+
+            $scope.showAdventure = function (adventure) {
+                console.log(adventure);
+                $mdDialog.show({
+                        controller: AdventureController,
+                        templateUrl: '/modules/core/views/showAdventure.template.html',
+                        parent: angular.element(document.body),
+                        clickOutsideToClose: true,
+                        locals: {
+                            adventure: adventure
+                        }
+                    })
+                    .then(function (answer) {
+                        $scope.status = answer;
+
+                        console.log(answer);
+                    }, function () {
+                        $scope.status = undefined;
+                    });
+            };
 
         }
 ]);
+
+
+function AdventureController($scope, $mdDialog, adventure) {
+    $scope.adventure = adventure;
+    $scope.hide = function () {
+        $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
+    $scope.save = function () {
+        $mdDialog.hide($scope.user.dream);
+    };
+}
