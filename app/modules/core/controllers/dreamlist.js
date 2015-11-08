6af31f8f2e935498cc0d783c64419f0f3cf59d93
theStrategist <raaghav@outlook.com>
2015-11-08 01:54:09 -0800

    DreamList Version4

--- app/modules/core/controllers/dreamlist.js
+++ app/modules/core/controllers/dreamlist.js
@@ -16,35 +16,35 @@ angular
 				[{
 					title:"Dubai",
 					imagePath:"../img/dubai.jpg",
-					content:"Dubai sits on a golden sandy coastline in the Arabian Gulf, where the warm azure waves of the sea meet the desert",
+					content:"",
 					visited:"true"
 				 },
 
 				 {
 					title:"Great Wall Of China",
 					imagePath:"../img/great_wall_china.jpg",
-					content:"The Great Wall of China is a series of fortifications made of stone, brick, tamped earth, wood, and other materials, generally built along an east-to-west line across the historical northern borders of China to protect the Chinese states and empires against the raids and invasions of the various nomadic groups of the Eurasian Steppe.",
+					content:"",
 					visited:"false"
 				 },
 
 				 {
 					title:"Yosemite",
 					imagePath:"../img/Yosemite.jpg",
-					content:"Yosemite National Park is set within California’s Sierra Nevada mountains. It’s famed for its giant, ancient sequoias, and for Tunnel View, the iconic vista of towering Bridalveil Fall and the granite cliffs of El Capitan and Half Dome.",
+					content:"",
 					visited:"false"
 				 },
 
 				 {
 					title:"Paris Art Gallery",
 					imagePath:"../img/Paris.jpg",
-					content:"Paris has long been synonymous with burgeoning arts and cultural flair. With such a wealth of public galleries, creative events and white spaces, no selection of the top 10 contemporary art galleries can be comprehensive.",
+					content:"",
 					visited:"false"
 				 },
 
 				 {
 					title:"Portugal",
 					imagePath:"../img/Portugal.jpg",
-					content:"Spread over a string of seven hills north of the Rio Tejo (Tagus River) estuary, Lisbon presents an intriguing variety of faces to those who negotiate its switchback streets. In the oldest neighborhoods, stepped alleys whose street pattern dates back to Moorish times are lined with pastel-color houses decked with laundry.",
+					content:"",
 				 	visited:"false"
 				 },
 
@@ -52,14 +52,14 @@ angular
 				 {
 					title:"The Nevis, Queenstown, New Zealand",
 					imagePath:"../img/Bungee_Jumping.jpg",
-					content:"While its 43-metre drop might now be considered light on scares by serial thrill-seekers, the Kawarau's status as the place where it all began is undisputed. More than 650,000 people have taken the plunge from this historic suspension bridge, with many dipping their heads in the stunning turquoise waters below.",
+					content:"",
 				 	visited:"true"
 				 },
 
 				 {
 					title:"Boom,Belgium",
 					imagePath:"../img/tomorrowland.jpg",
-					content:"While its 43-metre drop might now be considered light on scares by serial thrill-seekers, the Kawarau's status as the place where it all began is undisputed. More than 650,000 people have taken the plunge from this historic suspension bridge, with many dipping their heads in the stunning turquoise waters below.",
+					content:"",
 				 	visited:"false"
 				 }
 
